8e4118bba69a92e504bf807b8a8bd1141b746146
Merlin Jeyakumar <s.merlinjeyakumar@gmail.com>
2021-12-19 21:32:31 +0530

    commented code removed

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,8 +1,6 @@
 import {BaseResponseModel} from "../model/BaseResponseModel";
-import jwt from "jsonwebtoken";
 import config from "../config/Configuration";
 import {verify} from "../job/JwtJob";
-import {UserModel} from "../model/UserModel";
 
 
 class PostController {
