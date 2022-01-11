036bed3a1a0d9548770900f721b3cfb0ce76a9e4
Merlin Jeyakumar <s.merlinjeyakumar@gmail.com>
2022-01-11 13:54:09 +0530

    code reverted

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,21 +1,12 @@
 import {Request, Response} from "express";
-import {compareHashPassword, decryptStringAes, encryptStringAes, getPasswordHash} from "../support/EncryptionUtility";
+import {getRedisRefreshToken, putRedisRefreshToken} from "../job/RedisJob";
 
-/* https://github.com/brix/crypto-js/issues/271 */
+
 class PostController {
-    async addPost(req: Request, response: Response) { //todo: rewrite with source
-        const encryptedText = encryptStringAes("bujji harshi", "harshi");
-        console.log("JeyK: encryptedText: ", encryptedText)
-
-         const decryptedText = decryptStringAes(encryptedText, "harshi");
-         console.log("JeyK: decryptedText: ", decryptedText)
-
-        let plain = await getPasswordHash("Merlin")
-        console.log("Compare: ", await compareHashPassword("Merlin", plain))
-        //await putRedisRefreshToken("JeyK", "MyCuteToken")
-        //let value: string | undefined = await getRedisRefreshToken("JeyK")
-        //console.log("JeyK", `value ${value}`)
-        response.json("{}")
+    async addPost(req: Request, response: Response) {
+        await putRedisRefreshToken("JeyK", "MyCuteToken")
+        let value: string | undefined = await getRedisRefreshToken("JeyK")
+        console.log("JeyK", `value ${value}`)
         //todo: authorization
     }
 }
